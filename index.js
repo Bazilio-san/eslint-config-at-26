9913076f78e821b471eb653048d8f53f4b45c9b0
vvmakarov <vvmakarov@corp.finam.ru>
2025-12-16 10:20:57 +0300

    refactor: migrate to module-based structure, update package.json exports, and add LICENSE file

--- index.js
+++
@@ -1,6 +0,0 @@
-const flat = require('./eslint.config.js');
-
-// Export flat config as default (ESLint 9)
-module.exports = flat;
-// Also provide .flat for convenience
-module.exports.flat = flat;
